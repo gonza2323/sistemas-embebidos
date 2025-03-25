6d8f16a02443f564bd06eab339e27cfd764e9332
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-03-25 04:00:50 -0300

    Fix dependencies and basic frontend

--- tp1/main.py
+++ tp1/main.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask import url_for
 from flask import render_template
 from flask import request
+from livereload import Server
 
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/', methods=['GET'])
 def index():
  ##Código Python##
  return render_template('index.html')
+
+server = Server(app.wsgi_app)
+server.watch("templates/*.*") # or what-have-you
+server.watch("static/*.*") # or what-have-you
+server.serve(port=5000)
