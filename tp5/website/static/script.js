072b0230218e96c0beedc192064a275d0fd72559
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-04-11 02:14:32 -0300

    Finish tp5

--- tp5/website/static/script.js
+++ tp5/website/static/script.js
@@ -1,99 +1,83 @@
 "use strict";
 
 
+const CHART_DURATION = 60000;
+const MAX_INTERRUPTION_DURATION = 7500;
+
+
+let lastTimestamp;
+
+
+Chart.register(ChartStreaming);
 const socket = io("http://localhost:5000");
 
-let events = [];
 
-
-socket.on("single_event", (event) => {
-    events.push(event);
-    renderTable();
+const ctx = document.getElementById('chart').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'Iluminación',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgb(240, 51, 51)',
+            borderWidth: 2,
+            pointRadius: 2,
+            data: []
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Hora (UTC-3)'
+                },
+                type: 'realtime',
+                realtime: {
+                    duration: CHART_DURATION,
+                    refresh: 1000,
+                    delay: 1000,
+                    pause: false,
+                    ttl: CHART_DURATION * 2
+                },
+                ticks: {
+                    source: 'auto',
+                    autoSkip: true
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Iluminación (%)'
+                },
+                max: 100
+            }
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: 'Iluminación en función del tiempo'
+            },
+            legend: {
+                display: false
+            }
+        },
+        animation: {
+            duration: 100
+        }
+    }
 });
 
-socket.on("all_events", (receivedEvents) => {
-    events = receivedEvents;
-    renderTable();
-})
 
-async function updateTime(event) {
-    try {
-        const response = await fetch('sync-time', {
-            method: 'POST',
-        });
-
-        if (!response.ok)
-            throw new Error('Network response was not ok ' + response.statusText);
-
-    } catch (error) {
-        console.error('There was an error:', error);
-    }
-}
-
-async function requestEvents(event) {
-    try {
-        const response = await fetch('request-events', {
-            method: 'GET',
-        });
-
-        if (!response.ok)
-            throw new Error('Network response was not ok ' + response.statusText);
-
-    } catch (error) {
-        console.error('There was an error:', error);
-    }
-}
-
-async function eraseMemory(event) {
-    try {
-        const response = await fetch('erase-memory', {
-            method: 'POST',
-        });
-
-        if (!response.ok)
-            throw new Error('Network response was not ok ' + response.statusText);
-
-        events = [];
-        renderTable();
-
-    } catch (error) {
-        console.error('There was an error:', error);
-    }
-}
-
-function renderTable() {
-    const noEventsPlaceholder = document.querySelector('#no-events-placeholder');
-    const tbody = document.querySelector('tbody');
-    tbody.innerHTML = '';
-
-    if (events.length === 0) {
-        noEventsPlaceholder.hidden = false;
-        return;
-    }
-
-    const fragment = document.createDocumentFragment();
+socket.on('new_data_point', function (data) {
+    if (lastTimestamp && data.timestamp - lastTimestamp > MAX_INTERRUPTION_DURATION)
+        chart.data.datasets[0].data.push({ x: data.timestamp, y: null });
     
-    events.forEach(e => {
-        const row = document.createElement('tr');
-        
-        const event = row.insertCell();
-        const tiemstamp = row.insertCell();
-        const time = row.insertCell();
-
-        event.textContent = e.event;
-        tiemstamp.textContent = e.timestamp;
-        time.textContent = e.time;
-
-        fragment.prepend(row);
+    chart.data.datasets[0].data.push({
+        x: data.timestamp,
+        y: data.illumination
     });
 
-    noEventsPlaceholder.hidden = true;
-    tbody.appendChild(fragment);
-}
-
-document.querySelector('#update-time').addEventListener('click', updateTime);
-document.querySelector('#request-events').addEventListener('click', requestEvents);
-document.querySelector('#erase-memory').addEventListener('click', eraseMemory);
-
-requestEvents();
-renderTable();
+    lastTimestamp = data.timestamp;
+});
